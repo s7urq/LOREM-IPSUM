2bccfd7f383f76f5ac8f2b0f99409feb301a2b5c
s7urq <surquiza@itba.edu.ar>
2022-08-30 19:51:34 -0300

    flecha

--- poderes.js
+++ poderes.js
@@ -1,11 +1,7 @@
-
-const btnAbrir =
-document.querySelector("#abrir-popup");
-const btnCerrar =
-document.querySelector("#cerrar-popup");
-const modal =
-document.querySelector("#modal");
-
+console.log("Hola")
+const btnAbrir =document.querySelector("#abrir-popup");
+const btnCerrar =document.querySelector("#cerrar-popup");
+const modal =document.querySelector("#modal");
 
 btnAbrir.addEventListener("click",()=>{modal.ShowModal()
 })
