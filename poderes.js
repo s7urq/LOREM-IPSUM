550fc8338d9252d94cb87be0022c9eef5f791790
s7urq <surquiza@itba.edu.ar>
2022-08-30 12:30:56 -0300

    ni idea

---
+++ poderes.js
@@ -0,0 +1,13 @@
+
+const btnAbrir =
+document.querySelector("#abrir-popup");
+const btnCerrar =
+document.querySelector("#cerrar-popup");
+const modal =
+document.querySelector("#modal");
+
+
+btnAbrir.addEventListener("click",()=>{modal.ShowModal()
+})
+
+
